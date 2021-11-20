8118780119896525b8715422784e1bfed6298557
unknown <kjaehwan89@gmail.com>
2021-11-20 08:13:54 -0500

    Change LoginForm Layout

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -4,15 +4,19 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { LockFilled, UserOutlined } from '@ant-design/icons';
 import useInput from '../hooks/useInput';
 import { loginRequestAction } from '../reducers/user';
 
-const ButtonWrapper = styled.div`
-  margin-top: 10px;
-`;
-
 const FormWrapper = styled(Form)`
   padding: 10px;
+  div {
+    margin-top: 5px;
+  }
+  Button {
+    margin-top: 5px;
+    width: 100%;
+  }
 `;
 
 const LoginForm = () => {
@@ -35,14 +39,12 @@ const LoginForm = () => {
   return (
     <FormWrapper onFinish={onSubmitForm}>
       <div>
-        <label htmlFor="user-email">Email</label>
-        <br />
-        <Input name="user-email" type="email" value={email} onChange={onChangeEmail} required />
+        <Input placeholder="Email" prefix={<UserOutlined />} name="user-email" type="email" value={email} onChange={onChangeEmail} required />
       </div>
       <div>
-        <label htmlFor="user-password">Password</label>
-        <br />
         <Input
+          placeholder="Password"
+          prefix={<LockFilled />}
           name="user-password"
           type="password"
           value={password}
@@ -50,10 +52,8 @@ const LoginForm = () => {
           required
         />
       </div>
-      <ButtonWrapper>
-        <Button type="primary" htmlType="submit" loading={logInLoading}>Login</Button>
-        <Link href="/signup"><a><Button>Sign-Up</Button></a></Link>
-      </ButtonWrapper>
+      <Button type="primary" htmlType="submit" loading={logInLoading}>Login</Button>
+      <Link href="/signup"><a><Button>Sign-Up</Button></a></Link>
     </FormWrapper>
   );
 };
