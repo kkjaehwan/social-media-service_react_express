d3e8c0f2d2c95aa3c22d1117f2fde65f62ac2ebf
unknown <kjaehwan89@gmail.com>
2021-12-05 13:08:11 -0500

    fix serverside rendering

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -65,13 +65,13 @@ const Home = () => {
 // getStaticPath      : next 9.x
 // getServerSideProps : next 9.x 서버 사이드 랜더링이라도 값이 변하는 내용을 대상으로 함
 export const getServerSideProps = wrapper.getServerSideProps(
-  (store) => async ({ req }) => {
+  (store) => async (context) => {
     // 서버사이드 랜더링 로그인 등 쿠키 문제 해결
     // 브라우져에서 쿠키를 보내주는게 아니라 프론트 서버에서 보내주는 것임.
     // 프론트 서버에서 보관중인 쿠키를 back으로 전달해줌
-    const cookie = req ? req.headers.cookie : '';
+    const cookie = context.req ? context.req.headers.cookie : '';
     axios.defaults.headers.Cookie = '';
-    if (req && cookie) { // 쿠키 공유 문제 해결
+    if (context.req && cookie) { // 쿠키 공유 문제 해결
       axios.defaults.headers.Cookie = cookie;
     }
 
